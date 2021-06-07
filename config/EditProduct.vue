26ef94821bd6a74c9701c1a67418df17bc2b7163
lisavn <84811980+lisavn99@users.noreply.github.com>
2021-06-06 19:57:04 -0700

    Create EditProduct.vue

---
+++ config/EditProduct.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+        <my-header></my-header>
+    <h1> {{ $route.params.id}}</h1>
+    <h1> Edit User</h1>
+  </div>
+
+</template>
+<script>
+    export default {
+       
+    }
+</script>
